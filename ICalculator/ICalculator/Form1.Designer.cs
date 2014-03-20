6722ae8eefbda194956a86304a3a77bf1562be62
andrey <dred_95@mail.ru>
2014-03-20 10:35:51 +0700

    project commit
    main Form

---
+++ ICalculator/ICalculator/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.subtraction = new System.Windows.Forms.Button();
+            this.multiplication = new System.Windows.Forms.Button();
+            this.addition = new System.Windows.Forms.Button();
+            this.division = new System.Windows.Forms.Button();
+            this.firstField = new System.Windows.Forms.TextBox();
+            this.secondField = new System.Windows.Forms.TextBox();
+            this.resultField = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // subtraction
+            // 
+            this.subtraction.Location = new System.Drawing.Point(21, 184);
+            this.subtraction.Name = "subtraction";
+            this.subtraction.Size = new System.Drawing.Size(34, 30);
+            this.subtraction.TabIndex = 0;
+            this.subtraction.Text = "-";
+            this.subtraction.UseVisualStyleBackColor = true;
+            this.subtraction.Click += new System.EventHandler(this.substraction_Click);
+            // 
+            // multiplication
+            // 
+            this.multiplication.Location = new System.Drawing.Point(21, 229);
+            this.multiplication.Name = "multiplication";
+            this.multiplication.Size = new System.Drawing.Size(34, 30);
+            this.multiplication.TabIndex = 2;
+            this.multiplication.Text = "*";
+            this.multiplication.UseVisualStyleBackColor = true;
+            this.multiplication.Click += new System.EventHandler(this.multiplication_Click);
+            // 
+            // addition
+            // 
+            this.addition.Location = new System.Drawing.Point(72, 184);
+            this.addition.Name = "addition";
+            this.addition.Size = new System.Drawing.Size(34, 30);
+            this.addition.TabIndex = 3;
+            this.addition.Text = "+";
+            this.addition.UseVisualStyleBackColor = true;
+            this.addition.Click += new System.EventHandler(this.addition_Click);
+            // 
+            // division
+            // 
+            this.division.Location = new System.Drawing.Point(72, 229);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(34, 30);
+            this.division.TabIndex = 4;
+            this.division.Text = "/";
+            this.division.UseVisualStyleBackColor = true;
+            this.division.Click += new System.EventHandler(this.division_Click);
+            // 
+            // firstField
+            // 
+            this.firstField.Location = new System.Drawing.Point(21, 37);
+            this.firstField.Name = "firstField";
+            this.firstField.Size = new System.Drawing.Size(127, 20);
+            this.firstField.TabIndex = 5;
+            // 
+            // secondField
+            // 
+            this.secondField.Location = new System.Drawing.Point(21, 82);
+            this.secondField.Name = "secondField";
+            this.secondField.Size = new System.Drawing.Size(127, 20);
+            this.secondField.TabIndex = 6;
+            // 
+            // resultField
+            // 
+            this.resultField.AutoSize = true;
+            this.resultField.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultField.Location = new System.Drawing.Point(17, 127);
+            this.resultField.Name = "resultField";
+            this.resultField.Size = new System.Drawing.Size(48, 20);
+            this.resultField.TabIndex = 7;
+            this.resultField.Text = "result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 290);
+            this.Controls.Add(this.resultField);
+            this.Controls.Add(this.secondField);
+            this.Controls.Add(this.firstField);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.addition);
+            this.Controls.Add(this.multiplication);
+            this.Controls.Add(this.subtraction);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.MenuHighlight;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button subtraction;
+        private System.Windows.Forms.Button multiplication;
+        private System.Windows.Forms.Button addition;
+        private System.Windows.Forms.Button division;
+        private System.Windows.Forms.TextBox firstField;
+        private System.Windows.Forms.TextBox secondField;
+        private System.Windows.Forms.Label resultField;
+
+    }
+}
+
---
+++ ICalculator/ICalculator/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+    
+        private void substraction_Click(object sender, EventArgs e)
+        {
+            double firstArgument = Convert.ToDouble(firstField.Text);
+            double secondArgument = Convert.ToDouble(secondField.Text);
+            double result = firstArgument - secondArgument;
+            resultField.Text = result.ToString();
+        }
+    private void addition_Click(object sender, EventArgs e)
+        {
+            double firstArgument = Convert.ToDouble(firstField.Text);
+            double secondArgument = Convert.ToDouble(secondField.Text);
+            double result = firstArgument + secondArgument;
+            resultField.Text = result.ToString();
+        }
+
+    private void multiplication_Click(object sender, EventArgs e)
+    {
+        double firstArgument = Convert.ToDouble(firstField.Text);
+        double secondArgument = Convert.ToDouble(secondField.Text);
+        double result = firstArgument * secondArgument;
+        resultField.Text = result.ToString();
+    }
+
+    private void division_Click(object sender, EventArgs e)
+    {
+        double firstArgument = Convert.ToDouble(firstField.Text);
+        double secondArgument = Convert.ToDouble(secondField.Text);
+        double result = firstArgument / secondArgument;
+        resultField.Text = result.ToString();
+    }
+
+    }
+}
