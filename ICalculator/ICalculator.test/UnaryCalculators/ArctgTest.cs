6c3a21571347171836b923f83bc3c2ec82fe1578
SanGor <rapira_95@mail.ru>
2014-04-09 19:44:59 +0700

    Test operation Arctg

---
+++ ICalculator/ICalculator.test/UnaryCalculators/ArctgTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ICalculator.UnaryCalculators
+{
+     [TestFixture]
+    public class ArctgTest
+    {
+         [Test]
+         public void CalculateTest()
+         {
+             Arctg arctg = new Arctg();
+             Assert.AreEqual("98,93906527", arctg.Calculate(60), "arctg(60) !=1,0471975511966");
+         }
+    }
+}
