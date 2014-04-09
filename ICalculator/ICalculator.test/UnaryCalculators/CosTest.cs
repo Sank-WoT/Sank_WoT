6607b1cc24d59e02386195b921d7be166ed9cc0e
SanGor <rapira_95@mail.ru>
2014-04-09 22:28:14 +0700

    Test operation Cos

---
+++ ICalculator/ICalculator.test/UnaryCalculators/CosTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ICalculator.UnaryCalculators
+{
+    [TestFixture]
+    public class CosTest
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            Cos cos = new Cos();
+            Assert.AreEqual("0", cos.Calculate(1), "ln(1) !=0");
+        }
+    }
+}
