5e09a9d3d925582f287092c2d9083eacc1e322fd
123 <123>
2014-04-10 10:37:45 +0700

    Test operations Cot and Tan

---
+++ ICalculator/ICalculator.Tests/UnaryCalculators/CotTest.cs
@@ -0,0 +1,16 @@
+﻿using ICalculator.UnaryCalculators;
+using NUnit.Framework;
+
+namespace ICalculator.Tests.UnaryCalculators
+{
+     [TestFixture]
+    public class CotTest
+    {
+         [Test]
+         public void CalculateTest()
+         {
+             Cot cot = new Cot();
+             Assert.AreEqual(0.6421, cot.Calculate(1), 0.0001);
+         }
+    }
+}
